64a3e08357a851939d6f9edfbd2c81fb92e19f2e
Abdul Bashit <abashit87@gmail.com>
2024-06-13 17:39:43 +0100

    updated

--- jssession/script.js
+++ jssession/script.js
@@ -1,3 +1,43 @@
-alert("Hello World")
-        console.log("secret message")
-       
+// --diplaying messages in JS ---
+// alert("Hello World")
+//         console.log("secret message")
+
+        // --- Variables ---
+        //Fname, Lname, age, Gender, isEnrolled
+        //Variables created using camel casing
+        // let fName;
+        // let lName;
+        // // Numeric integer or float
+        // let age = 30;
+        // // String
+        // let gender;
+        // //Boolean data type
+        // let isEnrolled;
+        // //Unchangeable variable
+        // const scholl ="TCG";
+
+        // //Intilising fName
+        // fName ="my name";
+        // lName = "F";
+        //console.log("this name is: " fName + lName + "age is" + age + "studying in" school)
+        //template literals
+        //console.log(`this name is ${fName} ${lName} and age is ${age}`)
+
+        // // my name F is enrolled in TCG to learn web dev
+        // // + is used to combine the values --> concatenation operater
+        // console.log(fName + " " + lName)
+
+
+// let a = 5;
+// let b = 3;
+//Arthmethic operators:
+// let sum = a + b;
+// console.log("the sume of a and b is: " + sum) 
+// let sub = a - b;
+// console.log("the sume of a and b is: " + sub)
+// let mul = a * b;
+// console.log("the sume of a and b is: " + mul)
+
+// a+=5 is a =a + 5, always add 5 to which ever variable
+// a+= 5;
+// console.log(a);
